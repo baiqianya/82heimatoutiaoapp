a9f53fccab1adb8095e71e21632ff9a3af9facfa
baiqian <779875991@qq.com>
2019-09-18 23:56:10 +0800

    项目初始化---调整目录及导入Vant及封装axios及解决JS安全整数范围问题

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <router-view />
   </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+// 引入request
+import request from '@/utils/request'
+request({
+  method: 'GET',
+  url: '/app/v1_1/articles',
+  params: {
+    channel_id: 0,
+    timestamp: Date.now(),
+    with_top: 1
   }
+}).then(res => {
+  console.log(res.data)
+})
+
+export default {
+  name: 'App'
 }
+</script>
+
+<style>
 </style>
---
+++ src/utils/request.js
@@ -0,0 +1,19 @@
+import axios from 'axios' // 引入axios
+import JSONbig from 'json-bigint' // 引入json-bigint
+
+// 创建axios实例        .create 克隆一个axios实例
+const request = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn'
+})
+
+request.defaults.transformResponse =
+    [function (data) {
+      try {
+        return JSONbig.parse(data)
+      } catch (err) {
+        return data
+      }
+    }]
+
+// 导出request
+export default request
